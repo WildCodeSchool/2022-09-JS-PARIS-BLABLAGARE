1086d3e21df45e14cf10e995d690f2b95044f5ce
Rholivier <olivier.terral@sncf.fr>
2022-09-15 10:44:32 +0200

    actualizing

--- frontend/src/components/inscription/Inscription.jsx
+++ frontend/src/components/inscription/Inscription.jsx
@@ -1,39 +1,40 @@
-import React from 'react';
+import React from "react";
 import "./inscription.css";
 
-const Inscription = () => {
-    return (
-        <div className="inscription">
-
-            <form>
-                <label >
-                    Nom:
-                    <input id="firstName" type="text" name="firstName" />
-                </label>
-                <label  >
-                    Prénom:
-                    <input id="lastName" type="text" name="lastName" />
-                </label>
-                <label>
-                    Email:
-                    <input id="email" type="email" name="email" />
-                </label>
-                <label>
-                    Pseudo:
-                    <input id="name" type="text" name="alias" />
-                </label>
-                <label>
-                    Mot de passe:
-                    <input id="mdp" type="password" name="name" />
-                </label>
-                <label>
-                    Confirmation:
-                    <input id="confMdp" type="password" name="name" />
-                </label>
-                <button id="btn" type="submit">Valider</button>
-            </form>
-        </div>
-    );
-};
+function Inscription() {
+  return (
+    <div className="inscription">
+      <form>
+        <label>
+          Nom:
+          <input id="firstName" type="text" name="firstName" />
+        </label>
+        <label>
+          Prénom:
+          <input id="lastName" type="text" name="lastName" />
+        </label>
+        <label>
+          Email:
+          <input id="email" type="email" name="email" />
+        </label>
+        <label>
+          Pseudo:
+          <input id="name" type="text" name="alias" />
+        </label>
+        <label>
+          Mot de passe:
+          <input id="mdp" type="password" name="name" />
+        </label>
+        <label>
+          Confirmation:
+          <input id="confMdp" type="password" name="name" />
+        </label>
+        <button id="btn" type="submit">
+          Valider
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default Inscription;
