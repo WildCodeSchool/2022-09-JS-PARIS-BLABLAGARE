814e21fae298041485801425cb7fd151e954e906
jeremy1871 <j.perdrix.wcs@gmail.com>
2022-09-19 14:15:49 +0200

    update repo

--- frontend/src/components/acceuil/Acceuil.jsx
+++ frontend/src/components/acceuil/Acceuil.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import "./acceuil.css"; 
+import React from "react";
+import "./acceuil.css";
 
-const Acceuil = () => {
-    return (
-        <div className="acceuil">
-           <ul>
-		<li>Créer un compte</li>
-		<li>Me connecter</li>
-		   </ul> 
-        </div>
-    );
-};
+function Acceuil() {
+  return (
+    <div className="acceuil">
+      <ul>
+        <li>Créer un compte</li>
+        <li>Me connecter</li>
+      </ul>
+    </div>
+  );
+}
 
 export default Acceuil;
---
+++ frontend/src/pages/ValidateTrips.jsx
@@ -0,0 +1,13 @@
+import Background from "../components/background/Background";
+import Navbar from "../components/navBar/Navbar";
+import Confirmation from "../components/confirmation/Confirmation";
+
+export default function ValidateTrips() {
+  return (
+    <>
+      <Background />
+      <Navbar />
+      <Confirmation />
+    </>
+  );
+}
