6980b7499b24ebb54397cc7e3bd735f7aecff158
jeremy1871 <j.perdrix.wcs@gmail.com>
2022-09-19 14:11:42 +0200

    confirmation created

---
+++ frontend/src/components/confirmation/Confirmation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./confirmation.css";
+
+export default function confirmation() {
+  return (
+    <div className="confirmation">
+      <p>Offre bien prise en compte. </p>
+      <p>Merci User !</p>
+    </div>
+  );
+}
