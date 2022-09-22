f25d0f7bfaeba3caab9b4b24c57f32719c768d38
babols <julien.delpeut@laposte.net>
2022-09-22 17:03:35 +0200

    add page MyTrips

---
+++ frontend/src/pages/MyTrips/MyTrips.jsx
@@ -0,0 +1,14 @@
+import Background from "../../components/CardBackground/CardBackground";
+import Navbar from "../../components/CardNavBar/CardNavbar";
+import Result from "../../components/CardResult/CardResult";
+import "./MyTrips.css";
+
+export default function MyTrips() {
+  return (
+    <>
+      <Navbar />
+      <Result />
+      <Background />
+    </>
+  );
+}
